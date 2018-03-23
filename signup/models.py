e65446df564584366fc9a3cdd89b3a61a2e7ac90
BehlV10 <behlv10@gmail.com>
2018-03-23 18:42:54 -0400

    Updated Cities and Models to fix a Unix Bug

--- signup/models.py
+++ signup/models.py
@@ -17,13 +17,12 @@ from django.utils import timezone
 class WeatherSubscription(models.Model):
     email = models.EmailField(unique=True)
     city_choices = []
-    with open('util/cities.csv') as csvfile:
+    with open('util/cities.csv') as csvfile: #encoding="ISO-8859-1"
         city_input = csv.reader(csvfile)
         next(city_input)    # flush header line
         for i in range(0, 100):
             l = next(city_input)
             city_choices.append((str(l[1]) + "," + str(l[2]), str(l[1]) + ", " + str(l[2])))
-            next(city_input)    # flush every other line (just how file ended up being)
     
     city_choices = tuple([('', 'Select nearest city...')] + sorted(city_choices))
     
